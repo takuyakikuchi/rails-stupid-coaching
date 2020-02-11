1c625c02e18becba96f3fc3bb05aeb0a2ab5e902
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-02-11 16:33:50 +0900

    design updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # get 'questions/ask'
   # get 'questions/answer'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'questions#ask'
   get '/ask', to: 'questions#ask', as: :ask
   get '/answer', to: 'questions#answer', as: :answer
 end
