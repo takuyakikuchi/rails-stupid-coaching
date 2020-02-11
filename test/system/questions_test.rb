1ce661a638c322ef8df0240a133002fa4f630e79
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-02-11 17:28:26 +0900

    done testing

---
+++ test/system/questions_test.rb
@@ -0,0 +1,25 @@
+require "application_system_test_case"
+
+class QuestionsTest < ApplicationSystemTestCase
+  test "visiting /ask renders the form" do
+    visit ask_url
+    assert_selector "p", text: "Ask your coach anything"
+  end
+
+  test "saying Hello yields a grumpy response from the coach" do
+    visit ask_url
+    fill_in "question", with: "Hello"
+    click_on "Ask"
+
+    assert_text "I don't care, get dressed and go to work!"
+  end
+
+  test "saying what the coach is looking for" do
+    visit ask_url
+    fill_in "question", with: "I am going to work"
+    click_on "Ask"
+
+    assert_text "Great!"
+    take_screenshot
+  end
+end
